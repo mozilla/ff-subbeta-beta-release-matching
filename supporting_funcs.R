17bedddf93e46d23377fb005f583bb266c7596b7
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2019-09-23 17:55:16 -0700

    Adding report on validation and supporting files.

--- supporting_funcs.R
+++ supporting_funcs.R
@@ -6,7 +6,7 @@ library(tidyr)
 library(tidyselect)
 library(MatchIt)
 
-#### Helpers
+#### Helpers ####
 # TODO remove `tr_cov` arg
 calc_means <- function(df.match, tr_cov, ho_cov, add_1 = FALSE){
   # Validation
@@ -121,13 +121,16 @@ normalize_distro_id <- function(distro_id){
 }
 
 normalize_profile_age <- function(profile_age){
-  case_when(
-    profile_age < 7 ~ "< 1 week",
-    profile_age < 31 ~ "< 1 month",
-    profile_age < 180 ~ "< 6 months",
-    profile_age < (365 * 2) ~ "< 2 years",
-    profile_age < (365 * 5) ~ "< 5 years",
-    TRUE ~ "> 5 years"
+  levels <- c("< 1 week",  "< 1 month", "< 6 months", "< 2 years", "< 5 years", "> 5 years")
+  factor(
+    case_when(
+      profile_age < 7 ~ levels[1],
+      profile_age < 31 ~ levels[2],
+      profile_age < 180 ~ levels[3],
+      profile_age < (365 * 2) ~ levels[4], 
+      profile_age < (365 * 5) ~ levels[5],
+      TRUE ~ levels[6]
+    ), levels = levels, ordered = TRUE
   )
 }
 
@@ -136,41 +139,49 @@ normalize_timezone <- function(timezone){
 }
 
 normalize_memory <- function(memory){
+  levels <- c('< 1GB', '< 2GB', '< 4GB', '< 6GB', '< 16GB', '> 16GB')
   gb <- memory / 1024
-  case_when(
-    gb <= 1 ~ "< 1GB",
-    gb <= 2 ~ "< 2GB",
-    gb <= 4 ~ "< 4GB",
-    gb <= 6 ~ "< 6GB",
-    gb <= 16 ~ "< 16GB",
-    TRUE ~ "> 16GB"
+  factor(
+    case_when(
+      gb <= 1 ~ levels[1],
+      gb <= 2 ~ levels[2],
+      gb <= 4 ~ levels[3],
+      gb <= 6 ~ levels[4],
+      gb <= 16 ~ levels[5],
+      TRUE ~ levels[6]
+    ), levels = levels, ordered = TRUE
   )
 }
 
 normalize_cpu_speed <- function(cpu_speed){
+  levels <- c("< 1GHz", "< 2GHz", "< 3GHz", "< 4GHz", "> 16GHz")
   ghz <- cpu_speed / 1000
-  case_when(
-    ghz <= 1 ~ "< 1GHz",
-    ghz <= 2 ~ "< 2GHz",
-    ghz <= 3 ~ "< 3GHz",
-    ghz <= 4 ~ "< 4GHz",
-    TRUE ~ "> 16GHz"
+  factor(
+    case_when(
+      ghz <= 1 ~ levels[1],
+      ghz <= 2 ~ levels[2],
+      ghz <= 3 ~ levels[3],
+      ghz <= 4 ~ levels[4],
+      TRUE ~ levels[5]
+  ), levels = levels, ordered = TRUE
   )
 }
 
 normalize_cpu_cores <- function(cpu_cores){
   levels <- c('1', '2', '< 4', '< 8', '< 16', '> 16')
-  factor(case_when(
-    cpu_cores <= 1 ~ "1",
-    cpu_cores <= 2 ~ "2",
-    cpu_cores <= 4 ~ "< 4",
-    cpu_cores <= 8 ~ "< 8",
-    cpu_cores <= 16 ~ "< 16",
-    TRUE ~ "> 16"
-  ), levels = levels, ordered = TRUE)
+  factor(
+    case_when(
+      cpu_cores <= 1 ~ levels[1],
+      cpu_cores <= 2 ~ levels[2],
+      cpu_cores <= 4 ~ levels[3],
+      cpu_cores <= 8 ~ levels[4],
+      cpu_cores <= 16 ~ levels[5],
+      TRUE ~ levels[6]
+  ), levels = levels, ordered = TRUE
+  )
 }
 
-#### Diagnostics
+#### Diagnostics ####
 compare_log_cont <- function(df, covariate, tr_mean = NULL, tr_median = NULL, ho_mean = NULL, print = TRUE){
   p <- ggplot(df %>% mutate(!!covariate := get(covariate)+1), aes(label, get(covariate))) +
     geom_violin(aes(fill = label)) +
@@ -226,7 +237,7 @@ compare_cat <- function(df, covariate, limit = NULL, plot=TRUE, add_legend=TRUE)
   p <- ggplot(df_c, aes(category, density)) +
     geom_bar(aes(fill = label), position = "dodge", stat="identity") +
     theme_bw() +
-    theme(legend.position = c(0.2, 0.8)) +
+    theme(legend.position = c(0.2, 0.8), axis.text.x = element_text(angle = 45, hjust = 1)) +
     labs(x = covariate)
   
   if (!add_legend) p <- p + guides(fill=FALSE)
